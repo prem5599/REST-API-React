f0d7507e8bc98a152773f96e985ebff33abf75e7
prem5599 <prem5599@gmail.com>
2025-02-07 13:31:53 +0530

    fixxx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import CountryList from './components/CountryList/CountryList';
 import CountryDetails from './components/CountryDetails/CountryDetails';
@@ -8,28 +8,26 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="appwrapper">
-        <div className="bgeffects">
-          <div className="orb orb1"></div>
-          <div className="orb orb2"></div>
-          <div className="orb orb3"></div>
-        </div>
-
-        <Navbar />
-        
-        <main className="mainwrap">
-          <div className="contentwrap glasseffect">
-            <Routes>
-              <Route exact path="/" element={<CountryList />} />
-              <Route path="/country/:name" element={<CountryDetails />} />
-              <Route path="/favorites" element={<FavoritesList />} />
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </div>
-        </main>
+    <div className="appwrapper">
+      <div className="bgeffects">
+        <div className="orb orb1"></div>
+        <div className="orb orb2"></div>
+        <div className="orb orb3"></div>
       </div>
-    </Router>
+
+      <Navbar />
+      
+      <main className="mainwrap">
+        <div className="contentwrap glasseffect">
+          <Routes>
+            <Route exact path="/" element={<CountryList />} />
+            <Route path="/country/:name" element={<CountryDetails />} />
+            <Route path="/favorites" element={<FavoritesList />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </main>
+    </div>
   );
 }
 
